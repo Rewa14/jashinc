a89a1f95c52383a93ddeb55e68a1610ff162ba77
Rewa14 <jjlp98@gmail.com>
2015-12-10 02:06:36 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//All
+function Home() {
+    window.location.href = 'index.html';
+}
+
+function Vehicles() {
+    window.location.href = 'Aspect1.html';
+}
+
+function Computers() {
+    window.location.href = 'Aspect2.html';
+}
+
+function Pixar() {
+    window.location.href = 'Pixar.html';
+}
+
+function Campus() {
+    window.location.href = 'ThemeParks.html';
+}
+
+//Home
+loadBanner();
+
+var index=0;
+    function changeBanner(){ 
+      [].forEach.call(document.getElementsByClassName("galleryImages"),function (v,i) { document.getElementsByClassName("galleryImages")[i].hidden = i!==index});
+      index = (index+1) % document.getElementsByClassName("galleryImages").length;
+      console.log("test");    
+    }
+
+function loadBanner() {
+    var srcs = ["Images/P05.Campus1.jpg","Images/P06.Campus2.jpg","Images/P07.Campus3.jpg"];
+    var c = 0;
+    
+    imgs = [];
+    
+    for(i = srcs.length - 1; i >= 0; i--) {
+        imgs[i] = new Image();
+        imgs[i].src = srcs[i];
+        imgs[i].onload = function() {loaded();};
+    }
+    
+    loaded = function() {
+        c++;
+        if(c === srcs.length){
+            window.onload = function () {setInterval(changeBanner, 3000)};
+            changeBanner();
+        }
+    };
+    
+    this.update = function() {};
+}
+
+//Vehicles
+function toggleMe(a) {
+    var e = document.getElementById(a);
+    if(!e) return true;
+    if(e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+    return false;
+}
